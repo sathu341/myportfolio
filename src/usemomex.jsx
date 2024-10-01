17617dbc5270c981400706f95d227aba8c8e5b93
sathu341 <sthishchandran321@gmail.com>
2024-10-01 12:10:41 +0530

    six commit

--- src/usemomex.jsx
+++ src/usemomex.jsx
@@ -1,34 +1,32 @@
-import { useState,useMemo,useEffect} from "react";
+import { useState } from "react";
 function Usememoex(){
-    const [number,setNumber]=useState(0)
-    const [count,setCount]=useState(0);
-    const [square,setSquare]=useState(0)
+    const [number,setNumber]=useState(0);
+    const[count,setCount]=useState(0)
+    const incCount=()=>{
+        console.log("INC Count")
+        setCount(count+1)
+    }
+    const changeValue=(e)=>{
+        setNumber(e.target.value)
+    }
 
-   function incCount(){
-    console.log("incCount working")
-    setCount(count+1)
-   }
+   const sqaureResult=squareNumber(number)
 
-   
-   const changeValue=(e)=>{
-    setNumber(e.target.value)
-   }        
-   const squareResult=useMemo(()=>{
-    return squareNumber(number)
-   },[number])
+    function squareNumber(number){
+        console.log("square");
+        return Math.pow(Number(number),2)
+    }
     return(
         <>
-                                
-        
-          <input type="text" value={number}  onChange={changeValue}/>
-          <div style={{backgroundColor:'white'}}>output:{squareResult}</div>
-          <button onClick={incCount}>INC Count</button>
-          <div style={{backgroundColor:'white'}}>output:{count}</div>
+        <input type="text" onChange={changeValue} />
+          <div style={{backgroundColor:'white'}}>
+            {sqaureResult}
+          </div>
+        <button onClick={incCount}>INC Count</button>
+        <div style={{backgroundColor:'white'}}>
+            {count}
+        </div>
         </>
     )
 }
-function squareNumber(number){
-    console.log("square")
-    return Math.pow(Number(number),2)
-}
-export default Usememoex;                   
+export default Usememoex;
